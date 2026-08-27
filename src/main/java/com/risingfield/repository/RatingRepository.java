d9d9e0e9db283234f29a149e2041cc4fe8b2a66d
jaygopalka009 <jaygopalka009@gmail.com>
2026-08-27 11:12:21 +0530

    Update RatingRepository from main

--- src/main/java/com/risingfield/repository/RatingRepository.java
+++ src/main/java/com/risingfield/repository/RatingRepository.java
@@ -9,6 +9,4 @@ import java.util.Optional;
 public interface RatingRepository extends MongoRepository<Rating, Integer> {
     Optional<Rating> findByBookingId(Integer bookingId);
     List<Rating> findByTargetIdAndResourceType(Integer targetId, ResourceType resourceType);
-    List<Rating> findByFarmerId(Integer farmerId);
-    List<Rating> findByTargetId(Integer targetId);
 }
